25d9c8b4c2e4293dcaaf58787b3fbc6b02ac5a32
Timmmofey <tim.izmail@mail.ru>
2025-11-01 19:03:13 +0300

    Добавлена функция автоматической отправки языка в заголовке Accept-Language

--- src/lib/apiClientFactory.ts
+++ src/lib/apiClientFactory.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios"
+import { useLocaleStore } from "@/stores/localeStore"
 
 interface CreateApiClientOptions {
   baseURL: string
@@ -14,6 +15,22 @@ export function createApiClient({
   const client = axios.create({
     baseURL,
     withCredentials,
+  })
+
+  // interceptor для Accept-Language
+  client.interceptors.request.use((config) => {
+    config.headers = config.headers || {}
+
+    // клиентская локаль через getState()
+    if (typeof window !== "undefined") {
+      const locale = useLocaleStore.getState().locale
+      config.headers["Accept-Language"] = locale || navigator.language || "en"
+    } else {
+      // серверная локаль (SSR fallback)
+      config.headers["Accept-Language"] = "en"
+    }
+
+    return config
   })
 
   if (setupInterceptors) {
